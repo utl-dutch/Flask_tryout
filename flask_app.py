97ee4e6e3019b7857957580bce6dc48c7ce19b30
R. Uitterlinden <utl_webinfo@planet.nl>
2023-03-07 15:30:36 +0100

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
+@app.route('/')
+def home():
+    return "<p>Welkom op mijn nieuwe website!<p>"
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-    @app.route('/')
-    def home():
-        return "<p>Hello World!<p>"
+
+
